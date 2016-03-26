e25e25b3da82bb252884a4be4008d6ba648ee50f
David Stichter <david.stichter3414@gmail.com>
2016-03-26 13:34:44 -0400

    server.js added requires

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+
+var logger = require('morgan')
+
+var PORT = process.env.PORT || 8000
+
+var mongoose = require('mongoose')
+var db = 'mongodb://'
+
+app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'))
+
+app.get('*',function(req,res){
+  res.sendFile(process.cwd() + '/public/index.html')
+})
+
+app.listen(PORT, function(){
+  console.log('Listening on ', PORT);
+})
