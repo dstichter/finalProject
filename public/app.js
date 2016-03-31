7de5c1e5dbd851c6ddb371e74b5c5e64d9c8756a
d1son <dson120@gmail.com>
2016-03-31 18:59:55 -0400

    added search states to app.js for the search and searchResults page

--- public/app.js
+++ public/app.js
@@ -38,4 +38,38 @@ angular.module('beerApp', ['ui.router'])
           }
         }
       })
+      .state('search', {
+        url: "/search",
+        views : {
+          '':{
+            templateURL: '/views/search.html'
+          },
+          'nav@search': {
+            templateURL: '/views/assets/nav.html'
+          }, 
+          'body@search': {
+            '/views/search.html'
+          },
+          'footer@search': {
+            '/views/assets/footer.html'
+          }
+        }
+      })
+      .state('searchResults', {
+        url: "/searchResults",
+        views : {
+          '':{
+            templateURL: '/views/main.html'
+          },
+          'nav@searchResults': {
+            templateURL: '/views/assets/nav.html'
+          }, 
+          'body@searchResults': {
+            '/views/searchResults.html'
+          },
+          'footer@searchResults': {
+            '/views/assets/footer.html'
+          }
+        }
+      })
     }]);
