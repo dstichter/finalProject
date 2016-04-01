413296c1dd6fa5380e11d0774040da24d42e4731
Steven Buller <stev.buller@gmail.com>
2016-04-01 15:24:26 -0400

    bcrypt works on account creation and login

--- models/user.js
+++ models/user.js
@@ -1,26 +1,35 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcryptjs');
+var SALT_WORK_FACTOR = 10;
+
+var userSchema = new Schema({
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+});
 
 
 //attibuted to http://devsmash.com/blog/password-authentication-with-mongoose-and-bcrypt
-// userSchema.pre('save', function(next) {
-//     var user = this;
+userSchema.pre('save', function(next) {
+    var user = this;
 
-//     // only hash the password if it has been modified (or is new)
-//     if (!user.isModified('password')) return next();
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
 
-//     // generate a salt
-//     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//         if (err) return next(err);
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
 
-//         // hash the password using our new salt
-//         bcrypt.hash(user.password, salt, function(err, hash) {
-//             if (err) return next(err);
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
 
-//             // override the cleartext password with the hashed one
-//             user.password = hash;
-//             next();
-//         });
-//     });
-// });
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
 
 // UserSchema.methods.comparePassword = function(candidatePassword, cb) {
 //     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
@@ -28,3 +37,8 @@
 //         cb(null, isMatch);
 //     });
 // };
+
+
+
+
+module.exports = mongoose.model('User', userSchema);
