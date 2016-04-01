d548749d55355d4f596626d61e3478d60a1a6080
CB <cbmoate@gmail.com>
2016-04-01 16:16:00 -0400

    nothing to report

--- public/app.js
+++ public/app.js
@@ -1,41 +1,45 @@
-angular.module('beerApp', ['ui.router'])
-  .config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider){
-      $urlRouterProvider.otherwise('/');
-      $stateProvider
-      .state('home', {
-        url: '/',
-        views : {
-          '':{
-            templateURL: '/views/main.html'
-          }, 
-          'nav@home': {
-            templateURL: '/views/assets/nav.html'
-          }, 
-          'body@home': {
-            '/views/body.html'
-          },
-          'footer@home': {
-            '/views/assets/nav.html'
-          }
-        }
-      })
+angular.module('app', ['ui.router'])
 
-      .state('login', {
-        url: "/login",
-        views : {
-          '':{
-            templateURL: '/views/main.html'
-          },
-          'nav@login': {
-            templateURL: '/views/assets/nav.html'
-          }, 
-          'body@login': {
-            '/views/login.html'
-          },
-          'footer@login': {
-            '/views/assets/nav.html'
+  .config([
+    '$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
+      $urlRouterProvider.otherwise('/');
+      
+      $stateProvider
+        .state('home', {
+          url: '/',
+          views : {
+            '':{
+              templateURL: './views/main.html'
+            }, 
+            'nav@home': {
+              templateURL: './views/assets/nav.html'
+            }, 
+            'body@home': {
+              templateURL:'./views/body.html'
+            },
+            'footer@home': {
+              templateURL:'./views/assets/footer.html'
+            }
           }
-        }
-      })
+        })
+
+        .state('login', {
+          url: "/login",
+          views : {
+            '':{
+              templateURL: '/views/main.html'
+            },
+            'nav@login': {
+              templateURL: '/views/assets/nav.html'
+            }, 
+            'body@login': {
+              templateURL:'/views/login.html'
+            },
+            'footer@login': {
+              templateURL:'/views/assets/footer.html'
+            }
+          }
+        })
+
+      $locationProvider.html5Mode(true)
     }]);
