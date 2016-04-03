bf61be388cdf387cfa43bd9f5887baa0c8076df7
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-04-03 16:09:42 -0400

    added partial-login.html and worked on route redirection once the user is logged in

--- public/js/controllers/accountCtrl.js
+++ public/js/controllers/accountCtrl.js
@@ -1,15 +1,24 @@
-angular.module('app')
-.controller('accountController', function($scope, $http) {
+angular.module('beerApp')
+.controller('accountController', function($scope, $http, $rootScope, $location) {
   $scope.createAccount = function() {
     $http.post('/createAccount', {
       email: $scope.createEmail,
       password:$scope.createPassword,
       username:$scope.createUserName })
     .then(function(createResponse) {
+      if (createResponse.data === null) {
+        console.log("the user is saved");
+        console.log(createResponse.data);
+        $location.path('/');
+
+      } else {
+        console.log("the user exists");
+        console.log(createResponse.data);
+        $location.path('/');
+      }
       $scope.users = createResponse.data;
-      console.log(createResponse.data);
     });
-  }
+  };
 
   $scope.login = function() {
     $http.post('/login', {
@@ -17,13 +26,14 @@ angular.module('app')
       password:$scope.password1,
       username:$scope.inputUserName })
     .then(function(loginResponse) {
-      $scope.users1 = loginResponse.data;
-      //$scope.loggedIn = true;
-      // if ($scope.userName === loginResponse.data[0].name) {
-      //   $scope.loggedIn = true;
-      // }
-      console.log(loginResponse.data);
+      if (loginResponse.data.username) {
+      $rootScope.authenticated = true;
+      $rootScope.current_user = loginResponse.data.username;
+      $location.path('/');
+      console.log("the user is logged in");
+      console.log(loginResponse.data.username);
+      }
     });
-  }
+  };
 
 });
