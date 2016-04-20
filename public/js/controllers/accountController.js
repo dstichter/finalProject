919ecc06038f4beee0ee36e90482e2e28284f1db
Steven Buller <stev.buller@gmail.com>
2016-04-20 12:59:52 -0400

    removed console.log comments

--- public/js/controllers/accountController.js
+++ public/js/controllers/accountController.js
@@ -7,13 +7,8 @@ angular.module('beerApp')
       username:$scope.createUserName })
     .then(function(createResponse) {
       if (createResponse.data === null) {
-        //console.log("the user is saved");
-        //console.log(createResponse.data);
         $location.path('/login');
-
       } else {
-        //console.log("the user exists");
-        //console.log(createResponse.data);
         $location.path('/register');
         $scope.notRegistered = true;
       }
@@ -26,15 +21,11 @@ angular.module('beerApp')
       password:$scope.password1,
       username:$scope.inputUserName })
     .then(function(loginResponse) {
-      // console.log(loginResponse.status);
-      // console.log(loginResponse.data);
       if (loginResponse.data.username) {
       $rootScope.authenticated = true;
       $rootScope.current_user = loginResponse.data.username;
       $location.path('/profile');
-      //console.log("the user is logged in");
       } else {
-      //console.log("the user is not logged in");
       $scope.notLoggedIn = true;
       }
     });
