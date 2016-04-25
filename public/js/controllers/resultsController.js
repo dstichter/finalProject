eca253d7494fac88d7a39965c45c2a954a647b90
David Stichter <david.stichter3414@gmail.com>
2016-04-25 19:01:26 -0400

    Fixed seperating angular partials

--- public/js/controllers/resultsController.js
+++ public/js/controllers/resultsController.js
@@ -1,11 +1,26 @@
 angular.module('beerApp').controller('resultsController', function($scope, $http, $rootScope, $stateParams, apiResults, $timeout) {
   $scope.breweryApi = []
   $scope.beerApi = []
-  $scope.$watch(function () { return apiResults.get() }, function (newVal, oldVal) {
-    console.log(apiResults.get());
-    var hold = apiResults.get()
+  $scope.navbarApi = []
+  $scope.$watch(function () { return apiResults.get('brewery') }, function (newVal, oldVal) {
+    console.log(apiResults.get('brewery'));
+    var hold = apiResults.get('brewery')
       if (typeof newVal !== 'undefined') {
           $scope.breweryApi = hold.data;
       }
-  });
+  })
+  $scope.$watch(function () { return apiResults.get('beer') }, function (newVal, oldVal) {
+    console.log(apiResults.get('beer'));
+    var hold = apiResults.get('beer')
+      if (typeof newVal !== 'undefined') {
+          $scope.beerApi = hold.data;
+      }
+  })
+  $scope.$watch(function () { return apiResults.get('navbar') }, function (newVal, oldVal) {
+    console.log(apiResults.get('navbar'));
+    var hold = apiResults.get('navbar')
+      if (typeof newVal !== 'undefined') {
+          $scope.navbarApi = hold.data;
+      }
+  })
 })
